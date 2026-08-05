ea7f9b26dcabcabc825b26ca9ea602b5a260f7cf
Krishna <Milind.Krishna@McKesson.com>
2026-08-05 17:30:35 +0530

    v2 updated

--- test/conftest.py
+++ test/conftest.py
@@ -5,5 +5,5 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from dotenv import load_dotenv
 load_dotenv()
 
-# import deepeval
-# deepeval.login(api_key=os.environ.get("confident-api-key"))
+import deepeval
+deepeval.login(api_key=os.environ.get("confident-api-key"))
